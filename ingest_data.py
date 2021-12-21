797d1f438f57c069f77885b76957d56a1ea4ac83
nashay <nashay@yannix.com>
2021-12-21 19:11:13 +0700

    [Master] Add parameter

--- ingest_data.py
+++ ingest_data.py
@@ -4,14 +4,17 @@ import requests
 import json
 import time
 
+StartIndex = 710
+StopIndex = 770
+
 payload = {
 	"settings": {
 		"number_of_shards": 1,
-		"number_of_replicas": 0
+		"number_of_replicas": 1
 	}
 }
 
-for i in range( 981, 992 ):
+for i in range( StartIndex, StopIndex + 1 ):
 	response = requests.put( 'http://localhost:9200/test-index-{:06d}'.format( i ), json=payload )
 	if response.status_code == 200:
 		print( response.content )
